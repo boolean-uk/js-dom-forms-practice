eff2f9abe4af3a5b9b8ad6e4bf4cd37bbc221962
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-03-26 11:54:23 +0100

    core instructions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const form = document.querySelector("#form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const li = document.createElement("li");
+  li.innerText = `Name: ${name.value} 
+                  E-mail: ${email.value}`;
+
+  list.append(li);
+});
+
+console.log(form);
+console.log(name);
+console.log(email);
